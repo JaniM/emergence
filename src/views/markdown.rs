a33075fe5b58d58ecbc7ea3eff8b9850d420b656
Jani Mustonen <jani.mustonen@taitounited.fi>
2023-07-15 19:50:40 +0300

    styling

--- src/views/markdown.rs
+++ src/views/markdown.rs
@@ -2,11 +2,24 @@ use dioxus::prelude::*;
 
 #[inline_props]
 pub fn Markdown(cx: Scope, source: String) -> Element {
-    let html = markdown::to_html(&source);
-    render! {
-        div {
-            class: "markdown allow-select",
-            dangerous_inner_html: "{html}"
+    let html = markdown::to_html_with_options(&source, &markdown::Options::gfm());
+    let body = match html {
+        Ok(html) => rsx! {
+            div {
+                class: "markdown allow-select",
+                dangerous_inner_html: "{html}"
+            }
+        },
+        Err(e) => {
+            tracing::warn!("Markdown error: {}", e);
+            rsx! {
+                div {
+                    class: "markdown allow-select",
+                    "{source}"
+                }
+            }
         }
-    }
+    };
+
+    cx.render(body)
 }
