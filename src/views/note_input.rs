79c7f90df410e2c5878126354c02f9558dd65a3d
Jani Mustonen <jani.mustonen@taitounited.fi>
2023-08-22 21:03:47 +0300

    store note edit state in signals

--- src/views/note_input.rs
+++ src/views/note_input.rs
@@ -1,17 +1,128 @@
-use std::rc::Rc;
+use std::{hash::Hash, rc::Rc};
 
 use crate::{
     data::subjects::{Subject, SubjectId},
     views::{select_subject::SelectSubject, use_view_state, view_note::SubjectCards},
 };
+use ahash::HashMap;
 use dioxus::{
     html::input_data::keyboard_types::{Key, Modifiers},
     prelude::*,
 };
+use dioxus_signals::*;
 use emergence::data::{
     layer::use_layer,
-    notes::{Note, NoteBuilder, TaskState},
+    notes::{Note, NoteBuilder, NoteId, TaskState},
 };
+
+struct SignalCache<K, V: 'static> {
+    items: HashMap<K, Signal<V>>,
+    disposed_signals: Vec<Signal<V>>,
+}
+
+impl<K, V> Default for SignalCache<K, V> {
+    fn default() -> Self {
+        Self {
+            items: Default::default(),
+            disposed_signals: Default::default(),
+        }
+    }
+}
+
+impl<K, V> SignalCache<K, V>
+where
+    K: Default + Clone + Hash + Eq,
+    V: Default + 'static,
+{
+    fn grab(&mut self, scope: ScopeId, key: K) -> Signal<V> {
+        match self.items.entry(key) {
+            std::collections::hash_map::Entry::Vacant(entry) => {
+                let signal = if let Some(signal) = self.disposed_signals.pop() {
+                    tracing::trace!("Reusing existing signal");
+                    signal
+                } else {
+                    tracing::trace!("New signal");
+                    Signal::new_in_scope(Default::default(), scope)
+                };
+                *entry.insert(signal)
+            }
+            std::collections::hash_map::Entry::Occupied(entry) => *entry.get(),
+        }
+    }
+
+    fn dispose(&mut self, key: K, signal: Signal<V>) {
+        self.items.remove(&key);
+        self.disposed_signals.push(signal);
+        signal.set(Default::default());
+    }
+}
+
+struct CachedSignalOwner<K, V>
+where
+    K: 'static,
+    V: 'static,
+{
+    cache: Signal<SignalCache<K, V>>,
+    key: K,
+    signal: Signal<V>,
+}
+
+impl<K: Clone, V> Clone for CachedSignalOwner<K, V> {
+    fn clone(&self) -> Self {
+        Self {
+            cache: self.cache,
+            key: self.key.clone(),
+            signal: self.signal,
+        }
+    }
+}
+impl<K: Copy, V> Copy for CachedSignalOwner<K, V> {}
+
+impl<K, V> CachedSignalOwner<K, V>
+where
+    K: Default + Clone + Hash + Eq + std::fmt::Debug,
+    V: Default + 'static,
+{
+    fn dispose(self) {
+        tracing::trace!("Disposing signal for {:?}", self.key);
+        self.cache.write().dispose(self.key.clone(), self.signal);
+    }
+}
+
+impl<K, V> std::ops::Deref for CachedSignalOwner<K, V> {
+    type Target = Signal<V>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.signal
+    }
+}
+
+/// Gives a signal for the note editor's text.
+/// `note_id` *must not* change for the lifetime of the component.
+fn use_text_input_state(
+    cx: &ScopeState,
+    note_id: Option<NoteId>,
+    initial_text: Option<&str>,
+) -> CachedSignalOwner<Option<NoteId>, String> {
+    #[derive(Default, Clone)]
+    struct TextInputSignals(Signal<SignalCache<Option<NoteId>, String>>);
+
+    let cache = use_root_context(cx, TextInputSignals::default).0;
+    let scope = cache.origin_scope();
+    *cx.use_hook(|| {
+        let signal = cache.write().grab(scope, note_id);
+        if let Some(text) = initial_text {
+            if signal.read().is_empty() {
+                signal.set(text.to_owned());
+            }
+        }
+        CachedSignalOwner {
+            cache,
+            key: note_id,
+            signal,
+        }
+    })
+}
 
 #[derive(Props)]
 pub struct CreateNoteProps<'a> {
@@ -76,6 +187,7 @@ pub fn EditNote<'a>(cx: Scope<'a, EditNoteProps<'a>>) -> Element<'a> {
             on_cancel: on_done,
             initial_text: cx.props.note.text.clone(),
             initial_subjects: cx.props.note.subjects.clone(),
+            note_id: note_id,
         }
     })
 }
@@ -84,6 +196,7 @@ pub fn EditNote<'a>(cx: Scope<'a, EditNoteProps<'a>>) -> Element<'a> {
 struct NoteInputProps<'a> {
     on_create_note: EventHandler<'a, (String, Vec<SubjectId>)>,
     on_cancel: EventHandler<'a, ()>,
+    note_id: Option<NoteId>,
     initial_text: Option<String>,
     initial_subjects: Vec<SubjectId>,
 }
@@ -120,8 +233,9 @@ fn NoteInput<'a>(cx: Scope<'a, NoteInputProps<'a>>) -> Element<'a> {
         js_eval(TEXTAREA_HACK).unwrap();
     };
 
+    let text = use_text_input_state(cx, cx.props.note_id, cx.props.initial_text.as_deref());
+
     // TODO: Combine these states.
-    let text = use_state(cx, || cx.props.initial_text.clone().unwrap_or_default());
     let subjects = use_ref(cx, || cx.props.initial_subjects.clone());
     let show_subjects = use_state(cx, || ShowSubjects::No);
     let textarea = use_state(cx, || None::<Rc<MountedData>>);
@@ -133,15 +247,19 @@ fn NoteInput<'a>(cx: Scope<'a, NoteInputProps<'a>>) -> Element<'a> {
     };
 
     let submit = move || {
-        let text = text.get().trim().to_string();
         cleanup();
+        let trimmed = text.read().trim().to_string();
+        text.dispose();
         cx.props
             .on_create_note
-            .call((text, subjects.read().clone()));
+            .call((trimmed, subjects.read().clone()));
     };
 
     let cancel = move || {
         cleanup();
+        if &*text.read() == cx.props.initial_text.as_deref().unwrap_or_default() {
+            text.dispose();
+        }
         cx.props.on_cancel.call(());
     };
 
@@ -158,15 +276,14 @@ fn NoteInput<'a>(cx: Scope<'a, NoteInputProps<'a>>) -> Element<'a> {
         _ => {}
     };
 
-    let on_select_subject = |subject: Subject| {
+    let on_select_subject = move |subject: Subject| {
         subjects.write().push(subject.id);
         show_subjects.set(ShowSubjects::No);
         textarea.get().as_ref().unwrap().set_focus(true);
-        if *show_subjects.get() == ShowSubjects::YesKeyboard
-            && text.get().ends_with('@')
-        {
+        let t = text.read().clone();
+        if *show_subjects.get() == ShowSubjects::YesKeyboard && t.ends_with('@') {
             // remove the @
-            text.set(text.get()[0..text.get().len() - 1].to_string());
+            text.set(t[0..t.len() - 1].to_string());
         }
     };
 
@@ -185,10 +302,10 @@ fn NoteInput<'a>(cx: Scope<'a, NoteInputProps<'a>>) -> Element<'a> {
                 class: "note-content note",
                 textarea {
                     class: "note-textarea",
-                    value: "{text}",
+                    value: "{*text}",
                     rows: 2,
                     onmounted: move |e| {
-                        view_state.write().side_panel.list_similar(text.get().clone());
+                        view_state.write().side_panel.list_similar(text.read().clone());
                         textarea.set(Some(e.inner().clone()));
                         e.inner().set_focus(true);
                         size_textareas();
